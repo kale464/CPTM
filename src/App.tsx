dae4405127ed08b895bbd09ba3fc7c9a7780f701
unknown <Student@SIOP0206-EDU-22.EPTM.lan>
2025-05-06 16:35:19 +0200

    feat: udpated frontend to have all the elements needed

--- src/App.tsx
+++ src/App.tsx
@@ -9,35 +9,39 @@ function App() {
   };
 
   return (
-    <>
-      <h1>CPTM</h1>
-      <div className="main-section">
-        <div className="structures-panel">
-          <h2>Minijeux</h2>
-          <p>À venir...</p>
-        </div>
+    <div className="app-container">
+      <div className="game-layout">
+        <div className="main-content">
+          <h1>CPTM</h1>
+          <div className="main-section">
+            <div className="structures-panel">
+              <h2>Minijeux</h2>
+              <p>À venir...</p>
+            </div>
 
-        <div className="center-area">
-          <div className="card">
-            <button className="cookie-button" onClick={handleCookieClick}>
-              🍪
-            </button>
-            <p>Avis d'abscences: {cookies}</p>
+            <div className="center-area">
+              <div className="card">
+                <button className="cookie-button" onClick={handleCookieClick}>
+                  🍪
+                </button>
+                <p>Avis d'abscences: {cookies}</p>
+              </div>
+              <h2>Améliorations</h2>
+              <p>No upgrades yet. Stay tuned!</p>
+            </div>
+
+            <div className="structures-panel">
+              <h2>Structures</h2>
+              <p>Pas de structures</p>
+            </div>
           </div>
-          <h2>Améliorations</h2>
-          <p>No upgrades yet. Stay tuned!</p>
         </div>
 
-        <div className="structures-panel">
-          <h2>Structures</h2>
-          <p>Pas de structures</p>
-        </div>
+        <p className="read-the-docs">
+          Fait par Pedro Da Gloria Santiago, Leny Bressoud et Kaléa Righini
+        </p>
       </div>
-
-      <p className="read-the-docs">
-        Fait par Pedro Da Gloria Santiago, Leny Bressoud et Kaléa Righini
-      </p>
-    </>
+    </div>
   );
 }
 
