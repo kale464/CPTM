c32eb3c42302d1f1a93d2f9f8af7b620f79c701c
unknown <Student@SIOP0206-EDU-22.EPTM.lan>
2025-05-06 14:08:20 +0200

    feat: first ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cookies, setCookies] = useState(0);
+
+  const handleCookieClick = () => {
+    setCookies(prev => prev + 1);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>CPTM</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button className="cookie-button" onClick={handleCookieClick}>
+          🍪
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p>Total CPTM Cookies: {cookies}</p>
+      </div>
+      <div className="upgrades">
+        <h2>Upgrades</h2>
+        <p>No upgrades yet. Stay tuned!</p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Made with ❤️ with Pedro Da Gloria Santiago, Leny Bressoud and Kaléa Righini
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
