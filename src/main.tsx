d428326d7706cebb6c50bf17ea1754814250e9b1
unknown <Student@SIOP0206-EDU-22.EPTM.lan>
2025-05-13 14:36:10 +0200

    fix: 404 errors now lead to the home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
